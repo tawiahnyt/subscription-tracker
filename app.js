a033d89cb094fe616bf25e715da854a7f2b64b93
tawiah <tawiahin4k@gmail.com>
2025-02-11 15:12:32 +0000

    🚩 add cookie parser middleware and enhance request parsing

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import ConnectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
