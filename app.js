a6afb824ef104d365b8a8d1133ea33d9c015b60d
tawiah <tawiahin4k@gmail.com>
2025-02-11 15:10:04 +0000

    🚩 add error handling middleware for improved error responses

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import ConnectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
