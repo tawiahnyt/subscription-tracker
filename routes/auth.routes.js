329d4ea09992c2a94c32eac1fa022de5b5b5fcc8
tawiah <tawiahin4k@gmail.com>
2025-02-18 13:45:26 +0000

    add authentication controller with signUp, signIn, and signOut methods

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,12 @@
 /** @format */
 
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controllers";
 
 const authRouter = Router();
 
-authRouter.post("sign-up", (req, res) => res.send({ title: "Sign up" }));
-authRouter.post("sign-in", (req, res) => res.send({ title: "Sign in" }));
-authRouter.post("sign-out", (req, res) => res.send({ title: "Sign out" }));
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
