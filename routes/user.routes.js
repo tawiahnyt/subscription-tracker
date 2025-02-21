f148133116a785c200a10660ad40a7a7d9188940
tawiah <tawiahin4k@gmail.com>
2025-02-21 11:12:48 +0000

    :sparkles: add user controller with getUsers and getUser methods for user retrieval

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 /** @format */
 
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controllers.js"
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "GET all users" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "GET user details" }));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUser);
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE user" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE user" }));
