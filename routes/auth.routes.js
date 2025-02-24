8a4704ad6555476055dbed04a232fe19e2070ab9
tawiah <tawiahin4k@gmail.com>
2025-02-24 12:43:40 +0000

    :sparkles: implement signIn method for user authentication with JWT token generation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,12 @@
 /** @format */
 
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controllers.js";
+import { signIn, signUp } from "../controllers/auth.controllers.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
-// authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-in", signIn);
 // authRouter.post("/sign-out", signOut);
 
 export default authRouter;
